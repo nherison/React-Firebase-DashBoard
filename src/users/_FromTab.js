98c5bcfbaa816e793ab99d3c96a113ce447b226a
sjhumili <sjkunai@outlook.com>
2019-09-26 16:22:22 +0700

    no message

---
+++ src/users/_FromTab.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import {FormTab} from 'react-admin';
+import {_ImageSlideShow} from './_ImageSlideShow';
+
+export const _FormTab = ({record= [], source}) => {
+    
+    
+    return (
+        <FormTab label='pictures'>
+            <p>dfdf</p>
+        </FormTab>
+    );
+
+
+}
